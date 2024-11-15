827f95f411b0ea3a60ecf2d15826c53981d9fe59
stliliav <147162483+stliliav@users.noreply.github.com>
2024-11-15 13:19:28 +0300

    Update fridge_proj.py

--- fridge_proj.py
+++ fridge_proj.py
@@ -1,37 +1,90 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+import pygame
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import Qt, QTime
+from PyQt5.QtWidgets import QLineEdit, QApplication, QMainWindow, QPushButton, QMessageBox, QLabel
 from PyQt5.QtGui import QIcon
 
-def main_screen():
+def main_scr():
     app = QApplication([])
+    global main_win
     main_win = QMainWindow()
+    global add_btn
+
+    def click_to_add(self):
+        added = QMessageBox(main_win)
+        #added.show()
+        added.setText("What do you want to add?     \n\n\n\n")
+        global line
+        line = QLineEdit(added)
+        line.move(65,45)
+        save = QPushButton(added)
+        save.setText("Save")
+        save.setGeometry(QtCore.QRect(15, 135, 100, 30))
+        save.clicked.connect(save_product)
+        added.exec_()
+
+    def save_product(self):
+        food = line.text()
+        inside = QLabel(main_win)
+        inside.setText("Inside:\n {food}:")
+        #text = inside_font.render("Inside: \n{food}:" , 1, (255, 255, 255))
+        #window.blit(text, (10, 20))
+
+    def need_the_time(self):
+        time = QTime.currentTime()
+        show_time = QMessageBox(main_win)
+        show_time.setText("This is the time now: " + time.toString())
+        show_time.setGeometry(QtCore.QRect(275, 50, 120, 35))
+        show_time.exec_()
+        
     main_win.setWindowTitle("Fridgerator")
     main_win.setGeometry(650, 300, 700, 700)
     main_win.setWindowIcon(QIcon("icoon.png"))
 
     add_btn = QPushButton(main_win)
-    add_btn.setText("Add a product!")
-    add_btn.move(50, 150)
-
+    add_btn.setGeometry(QtCore.QRect(50, 150, 120, 35))
+    add_btn.setText("Add a product")
+    add_btn.clicked.connect(click_to_add)
+            
     remove_btn = QPushButton(main_win)
-    remove_btn.setText("Remove")
-    remove_btn.move(50,450)
+    remove_btn.setGeometry(QtCore.QRect(50, 450, 120, 35))
+    remove_btn.setText("Remove a product")
+    remove_btn.clicked.connect(is_clicked)
 
     ice_btn = QPushButton(main_win)
+    ice_btn.setGeometry(QtCore.QRect(500, 150, 120, 35))
     ice_btn.setText("Put some ice")
-    ice_btn.move(500, 150)
+    ice_btn.clicked.connect(is_clicked)
+            
 
     amount_btn = QPushButton(main_win)
-    amount_btn.setText("Set an amount")
-    amount_btn.move(500, 450)
+    amount_btn.setGeometry(QtCore.QRect(500, 450, 120, 35))
+    amount_btn.setText("Set the amount")
+    amount_btn.clicked.connect(is_clicked)
+
+    time_btn = QPushButton(main_win)
+    time_btn.setGeometry(QtCore.QRect(275, 75, 120, 35))
+    time_btn.setText("Check time")
+    time_btn.clicked.connect(need_the_time)
+    
+
 
     main_win.show()
     app.exec_()
+
+def is_clicked(self):
+    print("it works")
+
+#def need_the_time(self):
+    #show_time = QPushButton(main_win)
+    #show_time.setGeometry(QtCore.QRect(275, 50, 120, 35))
+    #show_time.setText("This is the time now: " + time.toString())
+    #print("This is the time now: " + time.toString())
+    
 
 class Product():
     def __init__(self, amount, type):
         self.amount = amount
         self.type = type
 
-
-main_screen()
+main_scr()
